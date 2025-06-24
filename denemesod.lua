266d4185dea196e1905291cb69dea24e79145ebb
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-06-25 00:26:32 +0300

    Delete denemesod.lua

--- denemesod.lua
+++
@@ -1,5 +0,0 @@
-local args = {
-	"Dark",
-	game:GetService("Players"):WaitForChild("CikoIata").Character
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DualityShootActivation"):FireServer(unpack(args))
