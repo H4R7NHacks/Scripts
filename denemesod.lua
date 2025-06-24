ab5a53fb3f0759e1b45853b573145ae721288781
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-06-25 00:02:52 +0300

    Update denemesod.lua

--- denemesod.lua
+++ denemesod.lua
@@ -1,5 +1,5 @@
 local args = {
-	"Light",
+	"Duel",
 	game:GetService("Players"):WaitForChild("CikoIata").Character
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DualityShootActivation"):FireServer(unpack(args))
