10b3f7c787d37409d19553f07750733d5cd1d6e0
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-06-24 19:31:22 +0300

    Update favoyunforork.lua

--- favoyunforork.lua
+++ favoyunforork.lua
@@ -3,14 +3,15 @@ if game.PlaceId == 15144787112 then
 
 
 	local vim = game:GetService("VirtualInputManager")
-	local ball = game.Workspace.Balls:GetDescendants()
 	local cooldown = false
 
 	if game.Players.LocalPlayer.Name == "Slow_pq" or game.Players.LocalPlayer.Name == "Rising_An4el" then
 		while wait(0.1) do
-
+			
 			game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_net@0.1.0").net:FindFirstChild("RE/PlayerWantsRematch"):FireServer()
-	
+
+			local ball = game.Workspace.Balls:GetDescendants()
+			
 			for i,v in pairs (ball) do
 				if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart") then
                     			if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
