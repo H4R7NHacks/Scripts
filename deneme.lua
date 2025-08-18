f87c5dd09830374f7ee71490575d62e489148e87
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-18 04:17:19 +0300

    Update deneme.lua

--- deneme.lua
+++ deneme.lua
@@ -4,18 +4,11 @@ local userId = "750724417931313171"
 local duelwebhook = "https://discord.com/api/webhooks/1406797634605285426/9igbro2jUHGS3-c7hbSDCGNHDjvuB4t6hGa28OC0LydDqVLAdX_XVDaxzdCWAbPp4TCr"
 local killwebhook = "https://discord.com/api/webhooks/1406805123300200489/sBKUaBTUXR901tTDh0kcVDkVPncF4gf-HUNkOT7va738Mn9yCjYB0f26IuNEO8eg8O8G"
 
-local function send(msg)
-	if game.PlaceId == 15144787112 then
+
+if game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 8493256621 then
+
+	local function send(msg)
     	request({
-        	Url = duelwebhook,
-        	Method = "POST",
-        	Headers = {["Content-Type"] = "application/json"},
-        	Body = HttpService:JSONEncode({
-            	content = "<@"..userId.."> " .. msg
-        	})
-    	})
-	elseif game.PlaceId == 13772394625 then
-		request({
         	Url = killwebhook,
         	Method = "POST",
         	Headers = {["Content-Type"] = "application/json"},
@@ -24,10 +17,6 @@ local function send(msg)
         	})
     	})
 	end
-end
-
-if game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 8493256621 then
-
 	
 	send(":ballot_box_with_check: Private Server'da kill farmı başlatılmıştır, kolay gelsin!")
 	
