bf5de7f4b37f0f15b8d91b3ef53e5457d0a8916a
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-18 19:51:00 +0300

    Update favoyunforork.lua

--- favoyunforork.lua
+++ favoyunforork.lua
@@ -8,9 +8,9 @@ if game.PlaceId == 15144787112 and game.Players.LocalPlayer.UserId == 3076333652
 			
 		for i,v in pairs (ball) do
 			if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart") then
-                    		if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
+                if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
 					game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DualityShootActivation"):FireServer("Light", game:GetService("Players"):WaitForChild("1sametfarm1").Character)
-            	   	 	end
+            	end
 			end
 		end
 	end
@@ -29,13 +29,13 @@ elseif game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 307633
 	
 		for i,v in pairs (ball) do
 			if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart") then
-                    		if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
+                if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
 					wait(0.5)
 					game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DualityShootActivation"):FireServer("Light", game:GetService("Players"):WaitForChild("1sametfarm1").Character)
 					wait(0.1)
 					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(30, 3, 30)
 					wait(10)
-            	   	 	end
+            	end
 			end
 		end
 	end
@@ -48,14 +48,14 @@ elseif game.PlaceId == 13772394625 then
 	
 		for i,v in pairs (ball) do
 			if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart") then
-                    		if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
+                if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
 					for i, y in pairs (ballspawn) do
 						if y.Name == "BALLSPAWN" then
 							wait(0.1)
 							game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = y.CFrame
 						end
 					end
-            	   	 	end
+            	end
 			end
 		end
 	end
