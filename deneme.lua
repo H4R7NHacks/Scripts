12a21d7f37272baf1243517d72567b4038f4c807
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-18 04:26:57 +0300

    Update deneme.lua

--- deneme.lua
+++ deneme.lua
@@ -4,10 +4,62 @@ local userId = "750724417931313171"
 local duelwebhook = "https://discord.com/api/webhooks/1406797634605285426/9igbro2jUHGS3-c7hbSDCGNHDjvuB4t6hGa28OC0LydDqVLAdX_XVDaxzdCWAbPp4TCr"
 local killwebhook = "https://discord.com/api/webhooks/1406805123300200489/sBKUaBTUXR901tTDh0kcVDkVPncF4gf-HUNkOT7va738Mn9yCjYB0f26IuNEO8eg8O8G"
 
+local function sendwelcomeforduel(msg)
+    	request({
+        	Url = duelwebhook,
+        	Method = "POST",
+        	Headers = {["Content-Type"] = "application/json"},
+        	Body = HttpService:JSONEncode({
+            	content = msg
+        	})
+    	})
+end
 
-if game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 8493256621 then
+local function sendwelcomeforkill(msg)
+    	request({
+        	Url = killwebhook,
+        	Method = "POST",
+        	Headers = {["Content-Type"] = "application/json"},
+        	Body = HttpService:JSONEncode({
+            	content = msg
+        	})
+    	})
+end
 
-	local function send(msg)
+local function sendforduelnp(msg)
+    	request({
+        	Url = duelwebhook,
+        	Method = "POST",
+        	Headers = {["Content-Type"] = "application/json"},
+        	Body = HttpService:JSONEncode({
+            	content = msg
+        	})
+    	})
+end
+
+local function sendforkillnp(msg)
+    	request({
+        	Url = killwebhook,
+        	Method = "POST",
+        	Headers = {["Content-Type"] = "application/json"},
+        	Body = HttpService:JSONEncode({
+            	content = msg
+        	})
+    	})
+end
+
+local function sendforduelcrash(msg)
+    	request({
+        	Url = duelwebhook,
+        	Method = "POST",
+        	Headers = {["Content-Type"] = "application/json"},
+        	Body = HttpService:JSONEncode({
+            	content = "<@"..userId.."> " .. msg
+        	})
+    	})
+end
+
+local function sendforkillcrash(msg)
     	request({
         	Url = killwebhook,
         	Method = "POST",
@@ -16,16 +68,19 @@ if game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 8493256621
             	content = "<@"..userId.."> " .. msg
         	})
     	})
-	end
+end
+
+if game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 8493256621 then
+
 	
-	send(":ballot_box_with_check: Private Server'da kill farmı başlatılmıştır, kolay gelsin!")
+	sendwelcomeforkill(":ballot_box_with_check: Private Server'da kill farmı başlatılmıştır, kolay gelsin!")
 	
 	while task.wait(60) do
     	local count = #Players:GetPlayers()
     	if count >= 3 then
-        	send(":white_check_mark: Şimdilik sorun yok gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
+        	sendforkillnp(":white_check_mark: Şimdilik sorun yok gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
 		else
-			send(":x: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
+			sendforkillcrash(":x: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
     	end
 	end
 
@@ -33,14 +88,14 @@ if game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 8493256621
 elseif game.PlaceId == 15144787112 and game.Players.LocalPlayer.UserId == 8493256621 then
 
 	
-	send(":ballot_box_with_check: Duel'de farm başlatılmıştır, kolay gelsin!")
+	sendwelcomeforduel(":ballot_box_with_check: Duel'de farm başlatılmıştır, kolay gelsin!")
 	
 	while task.wait(60) do
     	local count = #Players:GetPlayers()
     	if count >= 2 then
-        	send(":white_check_mark: Şimdilik sorun yok gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
+        	sendforduelnp(":white_check_mark: Şimdilik sorun yok gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
 		else
-			send(":x: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
+			sendforduelcrash(":x: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda "..count.." oyuncu kaldı!")
     	end
 	end
 
