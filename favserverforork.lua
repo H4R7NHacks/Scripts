05c3e718f091dfb7dff6fbb7731c84a7bac0b6ca
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-22 21:46:05 +0300

    Create favserverforork.lua

---
+++ favserverforork.lua
@@ -0,0 +1,3 @@
+if game.Players.LocalPlayer.UserId == 4145216416 or game.Players.LocalPlayer.UserId == 4145209772 or game.Players.LocalPlayer.UserId == 4145213881 or game.Players.LocalPlayer.UserId == 4145223783 then
+  game:GetService("TeleportService"):Teleport(15264892126, game.Players.LocalPlayer)
+end
