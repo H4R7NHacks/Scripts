eedf8826e36eabc7bb387242e1b917b2d50ac90a
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-24 16:24:55 +0300

    Update favserverforork.lua

--- favserverforork.lua
+++ favserverforork.lua
@@ -1,3 +1,3 @@
-if game.Players.LocalPlayer.UserId == 4145216416 or game.Players.LocalPlayer.UserId == 4145209772 or game.Players.LocalPlayer.UserId == 4145213881 or game.Players.LocalPlayer.UserId == 4145223783 then
+if game.Players.LocalPlayer.UserId == 4145216416 or game.Players.LocalPlayer.UserId == 4145209772 or game.Players.LocalPlayer.UserId == 4145213881 or game.Players.LocalPlayer.UserId == 4145223783 or game.Players.LocalPlayer.UserId == 3076333652 then
   game:GetService("TeleportService"):Teleport(15264892126, game.Players.LocalPlayer)
 end
