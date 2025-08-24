4abee328bfcac2618b12424cc3796a400a0bb407
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-24 16:23:49 +0300

    Update favoyunforork.lua

--- favoyunforork.lua
+++ favoyunforork.lua
@@ -4,7 +4,7 @@ local HttpService = game:GetService("HttpService")
 local userId = "1300452724953055335"
 local duelwebhook = "https://discord.com/api/webhooks/1406789282760233071/tvPPkgEbDBL8St-T9X9bfJo4iG_jkBkPfZThp75qimk6YzVxA55TyRanPRRlRnWl_j8h"
 local killwebhook = "https://discord.com/api/webhooks/1406789460515094639/zfNnIXOtryiH4XJbg30ttdqlUD2x_wBgtXhgISWIkcEl234-fjagJvd5Ssc9uBh0G9cx"
-local winwebhook = "https://discord.com/api/webhooks/1408505808299491590/gpuh4Rxz3zO8w7BX_ZZxiUtoxWswUNcxDSLKYXLvq7Niw5AaAG--Jbj4TvKbwL5BW8nW"
+local winwebhook = "https://discord.com/api/webhooks/1406789524356595813/MCUKkc-e3rvFC521ZygTJ862FFcYMc5stjbaEu64BjVzlrmRUEwFwxTr5k2XPQnyithW"
 
 local function sendwelcomeforduel(msg)
     	request({
@@ -182,7 +182,7 @@ if game.PlaceId == 15144787112 and game.Players.LocalPlayer.UserId == 3076333652
 				wait(0.1)
 				sendduelamount("Mevcut Kill sayınız : **"..game.Players.LocalPlayer.leaderstats.Elims.Value.."**")
 			else
-				sendforduelcrash(":x: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda **"..count.."** oyuncu kaldı! Eğer 60 saniye sonra mesaj gelmediyse ana hesabınızın oyunda olmadığı anlamına gelir.")
+				sendforduelcrash(":warning: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda **"..count.."** oyuncu kaldı! Eğer 60 saniye sonra mesaj gelmediyse ana hesabınızın oyunda olmadığı anlamına gelir.")
 				wait(0.1)
 				sendduelamount("Mevcut Kill sayınız : **"..game.Players.LocalPlayer.leaderstats.Elims.Value.."**")
     			end
@@ -228,7 +228,7 @@ elseif game.PlaceId == 13772394625 and game.Players.LocalPlayer.UserId == 307633
 				wait(0.1)
 				sendkillamount("Mevcut Kill sayınız : **"..game.Players.LocalPlayer.leaderstats.Elims.Value.."**")
 			else
-				sendforkillcrash(":x: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda **"..count.."** oyuncu kaldı! Eğer 60 saniye sonra mesaj gelmediyse ana hesabınızın oyunda olmadığı anlamına gelir.")
+				sendforkillcrash(":warning: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda **"..count.."** oyuncu kaldı! Eğer 60 saniye sonra mesaj gelmediyse ana hesabınızın oyunda olmadığı anlamına gelir.")
 				wait(0.1)
 				sendkillamount("Mevcut Kill sayınız : **"..game.Players.LocalPlayer.leaderstats.Elims.Value.."**")
     			end
@@ -255,7 +255,7 @@ elseif game.PlaceId == 13772394625 then
 		end
 	end
 
-elseif game.PlaceId == 15264892126 and game.Players.LocalPlayer.UserId == 4145216416 then
+elseif game.PlaceId == 15264892126 and game.Players.LocalPlayer.UserId == 3076333652 then
 
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/Circle"))()
 
@@ -266,10 +266,8 @@ elseif game.PlaceId == 15264892126 and game.Players.LocalPlayer.UserId == 414521
 			for i,v in pairs (ball) do
 				if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart") then
                				if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
-						wait(0.5)
-						vim:SendKeyEvent(true, Enum.KeyCode.Q, false, game)
-						wait(0.05)
-						vim:SendKeyEvent(false, Enum.KeyCode.Q, false, game)
+						game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DualityShootActivation"):FireServer("Light", game:GetService("Players"):WaitForChild("1sametfarm1").Character)
+						wait(10)
             				end
 				end
 			end
@@ -277,7 +275,7 @@ elseif game.PlaceId == 15264892126 and game.Players.LocalPlayer.UserId == 414521
 	end)
 
 	task.spawn(function()
-		sendwelcomeforwin(":fire: Win farmı başlatılmıştır, gazanız mübarek olsun!")
+		sendwelcomeforwin(":ballot_box_with_check: Win farmı başlatılmıştır, kolay gelsin!")
 		wait(0.1)
 		sendwinamount("Mevcut Win sayınız : **"..game.Players.LocalPlayer.leaderstats.Wins.Value.."**")
 	
@@ -288,7 +286,7 @@ elseif game.PlaceId == 15264892126 and game.Players.LocalPlayer.UserId == 414521
 				wait(0.1)
 				sendwinamount("Mevcut Win sayınız : **"..game.Players.LocalPlayer.leaderstats.Wins.Value.."**")
 			else
-				sendforwincrash(":x: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda **"..count.."** oyuncu kaldı! Eğer 60 saniye sonra mesaj gelmediyse ana hesabınızın oyunda olmadığı anlamına gelir.")
+				sendforwincrash(":warning: Görünüşe göre bazı hesapların **Crash** yemiş gibi görünüyor. Toplam sunucuda **"..count.."** oyuncu kaldı! Eğer 60 saniye sonra mesaj gelmediyse ana hesabınızın oyunda olmadığı anlamına gelir.")
 				wait(0.1)
 				sendwinamount("Mevcut Win sayınız : **"..game.Players.LocalPlayer.leaderstats.Wins.Value.."**")
     			end
@@ -300,7 +298,7 @@ elseif game.PlaceId == 15264892126 and game.Players.LocalPlayer.UserId == 414521
 
 		Players.PlayerAdded:Connect(function(player)
    			if not LocalPlayer:IsFriendsWith(player.UserId) then
-				sendplayernameforwin(":warning: Sunucuda **"..player.Name.."** kullanıcı adına sahip yabancı oyuncu tespit edildiği için güvenlik amaçlı tüm hesaplar oyundan atılmıştır!")
+				sendplayernameforwin(":x: Sunucuda **"..player.Name.."** kullanıcı adına sahip yabancı oyuncu tespit edildiği için güvenlik amaçlı tüm hesaplar oyundan atılmıştır!")
 				wait(0.1)
        				game:Shutdown()
     			end
