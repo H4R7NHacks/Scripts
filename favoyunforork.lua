9c9cab648f38655094a49f0f04aff3539adc4963
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-24 17:01:20 +0300

    Update favoyunforork.lua

--- favoyunforork.lua
+++ favoyunforork.lua
@@ -266,7 +266,13 @@ elseif game.PlaceId == 15264892126 and game.Players.LocalPlayer.UserId == 307633
 			for i,v in pairs (ball) do
 				if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart") then
                				if game.Workspace.Alive:FindFirstChild(game.Players.LocalPlayer.Name) then
+						local konum = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+						wait(0.1)
 						game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DualityShootActivation"):FireServer("Light", game.Players:GetPlayers()[math.random(1, #game.Players:GetPlayers())].Character)
+						wait(0.1)
+						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = konum
+						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = konum
+						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = konum
 						wait(10)
             				end
 				end
