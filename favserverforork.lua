855e58a6be24ad188c68de7f3b8ca3861ccc6268
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-09-09 22:00:47 +0300

    Update favserverforork.lua

--- favserverforork.lua
+++ favserverforork.lua
@@ -1,6 +1,6 @@
 if game.Players.LocalPlayer.UserId == 7660256590 or game.Players.LocalPlayer.UserId == 4145209772 or game.Players.LocalPlayer.UserId == 4145226469 or game.Players.LocalPlayer.UserId == 3076333652 then
-  --game:GetService("TeleportService"):Teleport(15264892126, game.Players.LocalPlayer)
-  game.Players.LocalPlayer:Kick("Gardaşşşşşşş benim yarra bi yala la, bizden mi gocunuyon gardaşşşşşşş :D")
+  game:GetService("TeleportService"):Teleport(15264892126, game.Players.LocalPlayer)
+  --game.Players.LocalPlayer:Kick("Gardaşşşşşşş benim yarra bi yala la, bizden mi gocunuyon gardaşşşşşşş :D")
 end
 
 -- 7660256590 anadolu_97
